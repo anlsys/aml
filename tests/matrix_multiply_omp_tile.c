b3545a0b23dbce07c616ddb54d3108770159bba8
Brian Suchy <BrianSuchy2022@u.northwestern.edu>
2018-07-11 11:20:44 -0500

    Surrounding code segments with preprocessors to completely isolate AML from MKL. Then going to get MKL version to run dgemm independently, then going to get AML to run dgemm seperately. FInally, get them to run simulateneously. ALso something of note is that MKL malloc does not support arrays larger than HBM capacity

--- tests/matrix_multiply_omp_tile.c
+++ tests/matrix_multiply_omp_tile.c
@@ -1,4 +1,3 @@
-#include <aml.h>
 #include <assert.h>
 #include <errno.h>
 #include <omp.h>
@@ -21,8 +20,16 @@
 #define PRINT_ARRAYS 0 
 #define HBM 1 
 #define DEBUG2 0 
-#define COMPINTEL 1
+#define INTEL 1
+#define ARGONNE 0
 
+#if ARGONNE == 1
+	#include <aml.h>
+	struct aml_tiling tiling, tilingB;
+	struct aml_area slow, fast;
+	struct aml_scratch sa, sb;
+
+#endif
 
 size_t numthreads;
 //size of 2D Tiles in A matrix
@@ -38,19 +45,24 @@ unsigned long long beginTime, endTime;
 unsigned long long waitingTime, totalWait;
 unsigned long long forkingTime, totalForkTime;
 clock_t startClock, endClock;
-AML_TILING_2D_DECL(tiling);
-AML_TILING_2D_DECL(tilingB);
-AML_AREA_LINUX_DECL(slow);
-AML_AREA_LINUX_DECL(fast);
-AML_SCRATCH_PAR_DECL(sa);
-AML_SCRATCH_PAR_DECL(sb);
+
+//AML_TILING_2D_DECL(tiling);
+//AML_TILING_2D_DECL(tilingB);
+//AML_AREA_LINUX_DECL(slow);
+//AML_AREA_LINUX_DECL(fast);
+//AML_SCRATCH_PAR_DECL(sa);
+//AML_SCRATCH_PAR_DECL(sb);
+
 //This code will take cycles executed as a use for timing the kernel.
-uint64_t rdtsc(){
+
+unsigned long rdtsc(){
     unsigned int lo,hi;
     __asm__ __volatile__ ("rdtsc" : "=a" (lo), "=d" (hi));
-    return ((uint64_t)hi << 32) | lo;
+    return ((unsigned long)hi << 32) | lo;
 }
 
+
+#if ARGONNE == 1
 //This temporary kernel will be our dgemm replacement.
 void multiplyTiles(double *a, double *b, double *c, int n, int m){
 	unsigned long i, j, k;
@@ -64,6 +76,7 @@ void multiplyTiles(double *a, double *b, double *c, int n, int m){
 	}
 
 }
+
 
 void do_work()
 {
@@ -230,13 +243,15 @@ void do_work()
 	
 }
 
+#endif
+
 
 //This matrix multiplication will implement matrix multiplication in the following way:
 //	The A, B, and C matrices will be broken into tiles that edge as close to 1 MB as possible (size of L2 cache) while also allowing all threads to work on data.
 //	The algorithm will chunk up the work dependent on number of tiles. The multiplication will go as follows:
 //		1) The algorithm will take a column of the tiles of A. (Prefetch next column of A tiles to fast memory).
 //		2) The algorithm will take a column of B tiles (prefetch next column into fast memory).
-//		3) Perform partial matrix multiplications using A tile and B Tile (using dgemm hopefully). 
+//		3) Perform partial matrix multiplications using A tile and B Tile (using dgemm eventually). 
 //		4) Repeat 2 & 3 until columns of B tiles are exhausted. Then continue
 //		5) Repeat from 1 until columns of A tiles are exhausted. Then continue
 //		6) DONE
@@ -244,11 +259,6 @@ void do_work()
 int main(int argc, char *argv[])
 {
 
-	AML_BINDING_SINGLE_DECL(binding);
-	AML_ARENA_JEMALLOC_DECL(arena);
-	AML_DMA_LINUX_SEQ_DECL(dma);
-	unsigned long nodemask[AML_NODEMASK_SZ];
-	aml_init(&argc, &argv);
 
 
 
@@ -271,19 +281,22 @@ int main(int argc, char *argv[])
 
 	}
 
-	if(COMPINTEL){
+	#if INTEL == 1
 		printf("Now running intel version for comparison\n");
 		double *aIntel, *bIntel, *cIntel;
-		int m,n,p;
+		unsigned int m,n,p;
 		unsigned long intelNumRows, intelEsize;
 		intelNumRows = (unsigned long)sqrt(MEMSIZE/8);
 		intelEsize = intelNumRows * intelNumRows;
 		m = n = p = intelNumRows;
 
 		printf("Declaring a, b, and c matrices\n");
-		aIntel = (double *)malloc( m*p*sizeof( double ) );
-    		bIntel = (double *)malloc( p*n*sizeof( double ) );
-    		cIntel = (double *)malloc( m*n*sizeof( double ) );
+		//aIntel = (double *)mkl_malloc( m*p*sizeof( double ), 64 );
+    		//bIntel = (double *)mkl_malloc( p*n*sizeof( double ), 64 );
+    		//cIntel = (double *)mkl_malloc( m*n*sizeof( double ), 64 );
+		aIntel = (double *)malloc(m*p*sizeof(double));
+		bIntel = (double *)malloc(p*n*sizeof(double));
+		cIntel = (double *)malloc(m*n*sizeof(double));
 		if (aIntel == NULL || bIntel == NULL || cIntel == NULL) {
         		printf( "\n ERROR: Can't allocate memory for matrices. Aborting... \n\n");
         		free(aIntel);
@@ -293,11 +306,13 @@ int main(int argc, char *argv[])
     		}
 		printf("Initializing values in the matrices\n");
 		double alpha = 1.0, beta = 1.0;
-		for(int i = 0; i < intelEsize; i++){
+		for(unsigned long i = 0; i < intelEsize; i++){
+			//printf("%lu ", i);
 			aIntel[i] = 1.0;
 			bIntel[i] = 1.0;
 			cIntel[i] = 0.0;
 		}
+		printf("m = %d, n = %d, p = %d, alpha = %lf, beta = %lf, aIntel = %p, bIntel = %p, cIntel = %p\n", m, n , p, alpha, beta, aIntel, bIntel, cIntel);
 		printf("Beginning cblas\n");
 		startClock = clock();
 		beginTime = rdtsc();
@@ -306,189 +321,208 @@ int main(int argc, char *argv[])
 		endTime = rdtsc();
 		endClock = clock();
 		printf("Intel Timing Statistics:\nRDTSC: %lu cycles\nCLOCK: %lf Seconds\n", endTime - beginTime, (double)(endClock - startClock) / CLOCKS_PER_SEC);
-	}
+	#endif
 	
-
+	#if ARGONNE == 1
+		AML_BINDING_SINGLE_DECL(binding);
+		AML_ARENA_JEMALLOC_DECL(arena);
+		AML_DMA_LINUX_SEQ_DECL(dma);
+		unsigned long nodemask[AML_NODEMASK_SZ];
+		aml_init(&argc, &argv);
 	
-	/* use openmp env to figure out how many threads we want
-	 * (we actually use 3x as much)
-	 */
-	esize = (unsigned long)MEMSIZE/sizeof(double);
-	numRows = (unsigned long)sqrt(esize);
+		AML_TILING_2D_DECL(tilingTemp);
+		AML_TILING_2D_DECL(tilingBTemp);
+		AML_AREA_LINUX_DECL(slowTemp);
+		AML_AREA_LINUX_DECL(fastTemp);
+		AML_SCRATCH_PAR_DECL(saTemp);
+		AML_SCRATCH_PAR_DECL(sbTemp);
 
-	if(VERBOSE);
-	#pragma omp parallel
-	{
-		rowLengthInBytes = (unsigned long)sqrt(MEMSIZE/sizeof(double)) * sizeof(double);
-		numthreads = omp_get_num_threads();
-		//CHUNKING = Total number of columns that will be handled by each thread
-		//Tilesz is found by dividing the length (in number of elements) of a dimension by the number of threads.
-		//It then checks to see if the tile is too large. If it is, then the size will be reduced until it will fit inside the L2 Cache
-		tilesz = ((unsigned long) pow( ( ( sqrt(MEMSIZE / sizeof(double)) ) / (numthreads * 2) ), 2) ) * sizeof(double);
-		double multiplier = 2;
-		while(tilesz > L2_CACHE_SIZE && argc != 4){
-			tilesz = (unsigned long)pow(((unsigned long)sqrt(tilesz/sizeof(double))/2), 2) * sizeof(double); 			
-			if(VERBOSE) printf("Resizing the tile size because it is too large for L2 cache. It is now of size: %lu\n", tilesz);
-		}
-		if(argc == 4){
-			tilesz = atol(argv[3]);
-		}
-		numTiles = MEMSIZE / tilesz; 
-		CHUNKING = numTiles / numthreads;
-		esz = tilesz/sizeof(double);
-		
-	}
-
-		
-	if(DEBUG) printf("Sizeof double: %d\n", sizeof(double));
-	if(DEBUG || VERBOSE)printf("The total memory size is: %lu\nWe are dealing with a %lu x %lu matrix multiplication\nThe number of threads: %d\nThe chunking is: %lu\nThe tilesz is: %lu\nThat means there are %lu elements per tile\nThere are %lu tiles total\nThe length of a column in bytes is: %lu\n", MEMSIZE, (unsigned long)sqrt(MEMSIZE/sizeof(unsigned long)), (unsigned long)sqrt(MEMSIZE/sizeof(unsigned long)),numthreads, CHUNKING, tilesz, esz, numTiles, rowLengthInBytes);
-
-	/* initialize all the supporting struct */
-	assert(!aml_binding_init(&binding, AML_BINDING_TYPE_SINGLE, 0));
-	assert(!aml_tiling_init(&tiling, AML_TILING_TYPE_2D, (unsigned long)sqrt(tilesz/sizeof(double))*sizeof(double), (unsigned long)sqrt(tilesz/sizeof(double))*sizeof(double), tilesz, MEMSIZE));	
-	rowSizeOfTile = aml_tiling_rowsize(&tiling, 0) / sizeof(double); 
-	rowSizeInTiles = numRows / rowSizeOfTile;
-	//This tiling B will be used for scratch pad memory movement of large tiled columns
-	assert(!aml_tiling_init(&tilingB, AML_TILING_TYPE_2D, rowSizeOfTile * sizeof(double), rowSizeInTiles * rowSizeOfTile * sizeof(double), tilesz * rowSizeInTiles, MEMSIZE));
-	if(DEBUG) printf("Row size in tiles = %lu\n", rowSizeInTiles);
-	AML_NODEMASK_ZERO(nodemask);
-	AML_NODEMASK_SET(nodemask, 0);
-	assert(!aml_arena_jemalloc_init(&arena, AML_ARENA_JEMALLOC_TYPE_REGULAR));
-
-	assert(!aml_area_linux_init(&slow,
-				    AML_AREA_LINUX_MANAGER_TYPE_SINGLE,
-				    AML_AREA_LINUX_MBIND_TYPE_REGULAR,
-				    AML_AREA_LINUX_MMAP_TYPE_ANONYMOUS,
-				    &arena, MPOL_BIND, nodemask));
-	AML_NODEMASK_ZERO(nodemask);
-	AML_NODEMASK_SET(nodemask, 1);
-	assert(!aml_area_linux_init(&fast,
-				    AML_AREA_LINUX_MANAGER_TYPE_SINGLE,
-				    AML_AREA_LINUX_MBIND_TYPE_REGULAR,
-				    AML_AREA_LINUX_MMAP_TYPE_ANONYMOUS,
-				    &arena, MPOL_BIND, nodemask));
+		tiling = tilingTemp;
+		tilingB = tilingBTemp;
+		slow = slowTemp;
+		fast = fastTemp;
+		sa = saTemp;
+		sb = sbTemp;
 	
-	assert(!aml_dma_linux_seq_init(&dma, 2));
-	//assert(!aml_dma_linux_seq_init(&dma, numthreads*2));
-	if(HBM){
-		if(DEBUG)printf("Declaring scratchpad for sa\n");
-		assert(!aml_scratch_par_init(&sa, &fast, &slow, &dma, &tilingB,
-				     2, 2));
-		//assert(!aml_scratch_par_init(&sa, &fast, &slow, &dma, &tilingB,
-		//		     2*numthreads, numthreads));
-
-		if(DEBUG)printf("Declaring scratchpad for sb\n");
-		assert(!aml_scratch_par_init(&sb, &fast, &slow, &dma, &tilingB,
-				     2, 2));
-		//assert(!aml_scratch_par_init(&sb, &fast, &slow, &dma, &tilingB,
-				    // 2*numthreads, numthreads));
-
-		if(DEBUG)printf("Sucessfully created both sa and sb\n");
-	}
-	/* allocation */
-	a = aml_area_malloc(&slow, MEMSIZE);
-	b = aml_area_malloc(&slow, MEMSIZE);
-	if(HBM){
-		c = aml_area_malloc(&fast, MEMSIZE);
-	}
-	else{
-		c = aml_area_malloc(&slow, MEMSIZE);
-	}
-	assert(a != NULL && b != NULL && c != NULL);
-	if(DEBUG)printf("Allocated space for a, b, and c matrices\n");
-	esize = MEMSIZE/sizeof(double);
-	numRows = (unsigned long)sqrt(esize);
-	for(unsigned long i = 0; i < esize; i++) {
-		a[i] = 1.0;//i % numRows;
-		b[i] = 1.0;//numRows - (i % numRows);
-		c[i] = 0.0;
-	}
-	if(VERBOSE) printf("esize = %lu\n", esize);
+		/* use openmp env to figure out how many threads we want
+		 * (we actually use 3x as much)
+		 */
+		esize = (unsigned long)MEMSIZE/sizeof(double);
+		numRows = (unsigned long)sqrt(esize);
 	
-	int newLines = 0;
-	if(PRINT_ARRAYS){
-		printf("A MATRIX:\n");
-		for(unsigned long i = 0; i < esize; i++) {
-			printf("%lf ", a[i]);
-			newLines++;
-			if(newLines == (unsigned long)sqrt(esize)){
-				printf("\n");
-				newLines = 0;
+		if(VERBOSE);
+		#pragma omp parallel
+		{
+			rowLengthInBytes = (unsigned long)sqrt(MEMSIZE/sizeof(double)) * sizeof(double);
+			numthreads = omp_get_num_threads();
+			//CHUNKING = Total number of columns that will be handled by each thread
+			//Tilesz is found by dividing the length (in number of elements) of a dimension by the number of threads.
+			//It then checks to see if the tile is too large. If it is, then the size will be reduced until it will fit inside the L2 Cache
+			tilesz = ((unsigned long) pow( ( ( sqrt(MEMSIZE / sizeof(double)) ) / (numthreads * 2) ), 2) ) * sizeof(double);
+			double multiplier = 2;
+			while(tilesz > L2_CACHE_SIZE && argc != 4){
+				tilesz = (unsigned long)pow(((unsigned long)sqrt(tilesz/sizeof(double))/2), 2) * sizeof(double); 			
+				if(VERBOSE) printf("Resizing the tile size because it is too large for L2 cache. It is now of size: %lu\n", tilesz);
 			}
-		}	
-		printf("\nB MATRIX:\n");
-	
-		for(unsigned long i = 0; i < esize; i++) {
-			printf("%lf ", b[i]);
-			newLines++;
-			if(newLines == (unsigned long)sqrt(esize)){
-				printf("\n");
-				newLines = 0;
+			if(argc == 4){
+				tilesz = atol(argv[3]);
 			}
+			numTiles = MEMSIZE / tilesz; 
+			CHUNKING = numTiles / numthreads;
+			esz = tilesz/sizeof(double);
+			
 		}
-		printf("\n");
-	}
 	
-
-	/* run kernel */
-	startClock = clock();
-	beginTime = rdtsc();
-
-	//BEGIN MULTIPLICATION
-//	#pragma omp parallel for
-//	for(unsigned long i = 0; i < numthreads; i++){
+			
+		if(DEBUG) printf("Sizeof double: %d\n", sizeof(double));
+		if(DEBUG || VERBOSE)printf("The total memory size is: %lu\nWe are dealing with a %lu x %lu matrix multiplication\nThe number of threads: %d\nThe chunking is: %lu\nThe tilesz is: %lu\nThat means there are %lu elements per tile\nThere are %lu tiles total\nThe length of a column in bytes is: %lu\n", MEMSIZE, (unsigned long)sqrt(MEMSIZE/sizeof(unsigned long)), (unsigned long)sqrt(MEMSIZE/sizeof(unsigned long)),numthreads, CHUNKING, tilesz, esz, numTiles, rowLengthInBytes);
+	
+		/* initialize all the supporting struct */
+		assert(!aml_binding_init(&binding, AML_BINDING_TYPE_SINGLE, 0));
+		assert(!aml_tiling_init(&tiling, AML_TILING_TYPE_2D, (unsigned long)sqrt(tilesz/sizeof(double))*sizeof(double), (unsigned long)sqrt(tilesz/sizeof(double))*sizeof(double), tilesz, MEMSIZE));	
+		rowSizeOfTile = aml_tiling_rowsize(&tiling, 0) / sizeof(double); 
+		rowSizeInTiles = numRows / rowSizeOfTile;
+		//This tiling B will be used for scratch pad memory movement of large tiled columns
+		assert(!aml_tiling_init(&tilingB, AML_TILING_TYPE_2D, rowSizeOfTile * sizeof(double), rowSizeInTiles * rowSizeOfTile * sizeof(double), tilesz * rowSizeInTiles, MEMSIZE));
+		if(DEBUG) printf("Row size in tiles = %lu\n", rowSizeInTiles);
+		AML_NODEMASK_ZERO(nodemask);
+		AML_NODEMASK_SET(nodemask, 0);
+		assert(!aml_arena_jemalloc_init(&arena, AML_ARENA_JEMALLOC_TYPE_REGULAR));
+	
+		assert(!aml_area_linux_init(&slow,
+					    AML_AREA_LINUX_MANAGER_TYPE_SINGLE,
+					    AML_AREA_LINUX_MBIND_TYPE_REGULAR,
+					    AML_AREA_LINUX_MMAP_TYPE_ANONYMOUS,
+					    &arena, MPOL_BIND, nodemask));
+		AML_NODEMASK_ZERO(nodemask);
+		AML_NODEMASK_SET(nodemask, 1);
+		assert(!aml_area_linux_init(&fast,
+					    AML_AREA_LINUX_MANAGER_TYPE_SINGLE,
+					    AML_AREA_LINUX_MBIND_TYPE_REGULAR,
+					    AML_AREA_LINUX_MMAP_TYPE_ANONYMOUS,
+					    &arena, MPOL_BIND, nodemask));
+		
+		assert(!aml_dma_linux_seq_init(&dma, 2));
+		//assert(!aml_dma_linux_seq_init(&dma, numthreads*2));
+		if(HBM){
+			if(DEBUG)printf("Declaring scratchpad for sa\n");
+			assert(!aml_scratch_par_init(&sa, &fast, &slow, &dma, &tilingB,
+					     2, 2));
+			//assert(!aml_scratch_par_init(&sa, &fast, &slow, &dma, &tilingB,
+			//		     2*numthreads, numthreads));
+	
+			if(DEBUG)printf("Declaring scratchpad for sb\n");
+			assert(!aml_scratch_par_init(&sb, &fast, &slow, &dma, &tilingB,
+					     2, 2));
+			//assert(!aml_scratch_par_init(&sb, &fast, &slow, &dma, &tilingB,
+					    // 2*numthreads, numthreads));
+	
+			if(DEBUG)printf("Sucessfully created both sa and sb\n");
+		}
+		/* allocation */
+		a = aml_area_malloc(&slow, MEMSIZE);
+		b = aml_area_malloc(&slow, MEMSIZE);
+		if(HBM){
+			c = aml_area_malloc(&fast, MEMSIZE);
+		}
+		else{
+			c = aml_area_malloc(&slow, MEMSIZE);
+		}
+		assert(a != NULL && b != NULL && c != NULL);
+		if(DEBUG)printf("Allocated space for a, b, and c matrices\n");
+		esize = MEMSIZE/sizeof(double);
+		numRows = (unsigned long)sqrt(esize);
+		for(unsigned long i = 0; i < esize; i++) {
+			a[i] = 1.0;//i % numRows;
+			b[i] = 1.0;//numRows - (i % numRows);
+			c[i] = 0.0;
+		}
+		if(VERBOSE) printf("esize = %lu\n", esize);
+		
+		int newLines = 0;
+		if(PRINT_ARRAYS){
+			printf("A MATRIX:\n");
+			for(unsigned long i = 0; i < esize; i++) {
+				printf("%lf ", a[i]);
+				newLines++;
+				if(newLines == (unsigned long)sqrt(esize)){
+					printf("\n");
+					newLines = 0;
+				}
+			}	
+			printf("\nB MATRIX:\n");
+		
+			for(unsigned long i = 0; i < esize; i++) {
+				printf("%lf ", b[i]);
+				newLines++;
+				if(newLines == (unsigned long)sqrt(esize)){
+					printf("\n");
+					newLines = 0;
+				}
+			}
+			printf("\n");
+		}
+		
+	
+		/* run kernel */
+		startClock = clock();
+		beginTime = rdtsc();
+	
+		//BEGIN MULTIPLICATION
+		//	#pragma omp parallel for
+		//	for(unsigned long i = 0; i < numthreads; i++){
 		do_work();
-//	}
-	//END MULTIPLICATION
-	endTime = rdtsc();
-	endClock = clock();
-	printf("Kernel Timing Statistics:\nRDTSC: %lu cycles\nCLOCK: %lf Seconds\nCycles waiting on memory: %lu\nCycles waiting to fork: %lu\n", endTime - beginTime, (double)(endClock - startClock) / CLOCKS_PER_SEC, totalWait, totalForkTime);
-
-	/* validate */
-	unsigned long correct = 1;
-	for(unsigned long i = 0; i < esize; i++){
-		if(c[0] != c[i]){
-			correct = 0;
-		}
-	}
-
-	if(PRINT_ARRAYS){
-		printf("esize = %lu\n", esize);
-		newLines = 0;
-		for(unsigned long i = 0; i < esize; i++) {
-			printf("%lf ", c[i]);
-			newLines++;
-			if(newLines == (unsigned long)sqrt(esize)){
-				printf("\n");
-				newLines = 0;
+		//	}
+		//END MULTIPLICATION
+		endTime = rdtsc();
+		endClock = clock();
+		printf("Kernel Timing Statistics:\nRDTSC: %lu cycles\nCLOCK: %lf Seconds\nCycles waiting on memory: %lu\nCycles waiting to fork: %lu\n", endTime - beginTime, (double)(endClock - startClock) / CLOCKS_PER_SEC, totalWait, totalForkTime);
+	
+		/* validate */
+		unsigned long correct = 1;
+		for(unsigned long i = 0; i < esize; i++){
+			if(c[0] != c[i]){
+				correct = 0;
 			}
 		}
-		printf("\n");
-	}
-
-	if(!correct){
-		printf("The matrix multiplication failed. The last incorrect result is at location C(0,0) = %lf in the C matrix\n", c[0]);
-	}
-	else{
-		printf("The matrix multiplication suceeded, C(0,0) = %lf\n", c[0]);
-	}
-
+	
+		if(PRINT_ARRAYS){
+			printf("esize = %lu\n", esize);
+			newLines = 0;
+			for(unsigned long i = 0; i < esize; i++) {
+				printf("%lf ", c[i]);
+				newLines++;
+				if(newLines == (unsigned long)sqrt(esize)){
+					printf("\n");
+					newLines = 0;
+				}
+			}
+			printf("\n");
+		}
+	
+		if(!correct){
+			printf("The matrix multiplication failed. The last incorrect result is at location C(0,0) = %lf in the C matrix\n", c[0]);
+		}
+		else{
+			printf("The matrix multiplication suceeded, C(0,0) = %lf\n", c[0]);
+		}
+	
 	
 
-	if(HBM) aml_scratch_par_destroy(&sa);
-	if(HBM) aml_scratch_par_destroy(&sb);
-	if(HBM) aml_dma_linux_seq_destroy(&dma);
-	aml_area_free(&slow, a);
-	aml_area_free(&slow, b);
-	if(HBM) aml_area_free(&fast, c);
-	if(!HBM) aml_area_free(&slow, c);
-	aml_area_linux_destroy(&slow);
-	if(HBM) aml_area_linux_destroy(&fast);
-	aml_tiling_destroy(&tiling, AML_TILING_TYPE_1D);
-	aml_tiling_destroy(&tilingB, AML_TILING_TYPE_2D);
-	aml_binding_destroy(&binding, AML_BINDING_TYPE_SINGLE);
-	aml_finalize();
-
+		if(HBM) aml_scratch_par_destroy(&sa);
+		if(HBM) aml_scratch_par_destroy(&sb);
+		if(HBM) aml_dma_linux_seq_destroy(&dma);
+		aml_area_free(&slow, a);
+		aml_area_free(&slow, b);
+		if(HBM) aml_area_free(&fast, c);
+		if(!HBM) aml_area_free(&slow, c);
+		aml_area_linux_destroy(&slow);
+		if(HBM) aml_area_linux_destroy(&fast);
+		aml_tiling_destroy(&tiling, AML_TILING_TYPE_1D);
+		aml_tiling_destroy(&tilingB, AML_TILING_TYPE_2D);
+		aml_binding_destroy(&binding, AML_BINDING_TYPE_SINGLE);
+		aml_finalize();
+	#endif
 
 
 
