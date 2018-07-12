5d9a2e0e01c7791417c124428512e16d468a5e1a
Brian Suchy <BrianSuchy2022@u.northwestern.edu>
2018-07-12 12:45:00 -0500

    Updated icc compiler file. Uses more flags, but still isn't fixing the hugetlb_init_once issue

--- tests/MMBuildIcc.sh
+++ tests/MMBuildIcc.sh
@@ -1,6 +1,5 @@
-icc -std=gnu99 -DHAVE_CONFIG_H -I. -I../src -O2 -fopenmp -mkl -static-intel -I../src -g -O2 -MT matrix_multiply_omp_tile.o -MD -MP -MF .deps/matrix_multiply_omp_tile.Tpo -c -o matrix_multiply_omp_tile.o matrix_multiply_omp_tile.c
+icc -std=gnu99 -DHAVE_CONFIG_H -I. -I../src -O3 -qopenmp -xCORE-AVX2 -mkl -static-intel -qopt-report-phase=vec  -liomp5 -lpthread -lm -ldl -I../src -O3 -MT matrix_multiply_omp_tile.o -MD -MP -MF .deps/matrix_multiply_omp_tile.Tpo -c -o matrix_multiply_omp_tile.o matrix_multiply_omp_tile.c
 
 mv -f .deps/matrix_multiply_omp_tile.Tpo .deps/matrix_multiply_omp_tile.Po
 
-/bin/sh ../libtool  --tag=CC   --mode=link icc -std=gnu99 -O2 -fopenmp -mkl -static-intel -I../src -g -O2 -fopenmp ../src/libaml.la ../jemalloc/lib/libjemalloc-aml.so  -o matrix_multiply_omp_tile matrix_multiply_omp_tile.o  -lnuma
-
+/bin/sh ../libtool  --tag=CC   --mode=link icc -std=gnu99 -O3 -qopenmp -xCORE-AVX2 -mkl -static-intel -qopt-report-phase=vec -I../src -O3 -mkl -xCORE-AVX2 -static-intel -qopenmp -qopt-report-phase=vec ../src/libaml.la ../jemalloc/lib/libjemalloc-aml.so  -o matrix_multiply_omp_tile matrix_multiply_omp_tile.o  -lnuma -Wl,--start-group ${MKLROOT}/lib/intel64/libmkl_intel_ilp64.a ${MKLROOT}/lib/intel64/libmkl_intel_thread.a ${MKLROOT}/lib/intel64/libmkl_core.a -Wl,--end-group -liomp5 -lpthread -lm -ldl 
