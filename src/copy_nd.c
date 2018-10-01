09fa767911881aa7d22d4d14b86f8a2083e37a6d
Brice Videau <brice.videau@imag.fr>
2018-10-01 12:42:04 -0500

    Simplified copy_nd.

--- src/copy_nd.c
+++ src/copy_nd.c
@@ -2,16 +2,25 @@
 #include <assert.h>
 #include <stdlib.h>
 
-static inline int aml_copy_2d(void *dst, const size_t *dst_pitch, const void *src, const size_t *src_pitch, const size_t *elem_number, size_t elem_size)
+static inline int aml_copy_2d(void *dst, const size_t *cumul_dst_pitch, const void *src, const size_t *cumul_src_pitch, const size_t *elem_number, size_t elem_size)
 {
-        assert( dst_pitch[0] >= elem_number[0] );
-        assert( src_pitch[0] >= elem_number[0] );
-        size_t dst_product_pitch = elem_size * dst_pitch[0];
-        size_t src_product_pitch = elem_size * src_pitch[0];
         for(int i = 0; i < elem_number[1]; i++) {
                 memcpy(dst, src, elem_number[0]*elem_size);
-                dst += dst_product_pitch;
-                src += src_product_pitch;
+                dst += cumul_dst_pitch[0];
+                src += cumul_src_pitch[0];
+        }
+        return 0;
+}
+
+static inline int aml_copy_nd_helper(size_t d, void *dst, const size_t *cumul_dst_pitch, const void *src, const size_t *cumul_src_pitch, const size_t *elem_number, const size_t elem_size) {
+        if(d == 2)
+                aml_copy_2d(dst, cumul_dst_pitch, src, cumul_src_pitch, elem_number, elem_size);
+        else {
+                for(int i = 0; i < elem_number[d-1]; i++) {
+                        aml_copy_nd_helper(d-1, dst, cumul_dst_pitch, src, cumul_src_pitch, elem_number, elem_size);
+                        dst += cumul_dst_pitch[d-2];
+                        src += cumul_src_pitch[d-2];
+                }
         }
         return 0;
 }
@@ -20,27 +29,26 @@ int aml_copy_nd(size_t d, void *dst, const size_t *dst_pitch, const void *src, c
 {
         assert(d > 0);
         if(d == 1)
+        {
                 memcpy(dst, src, elem_number[0]*elem_size);
-        else if(d == 2)
-                aml_copy_2d(dst, dst_pitch, src, src_pitch, elem_number, elem_size);
-        else {
-                assert( dst_pitch[d-2] >= elem_number[d-2] );
-                assert( src_pitch[d-2] >= elem_number[d-2] );
-
-                size_t dst_product_pitch = elem_size;
-                size_t src_product_pitch = elem_size;
-                for(int i = 0; i < d - 1; i++) {
-                        dst_product_pitch *= dst_pitch[i];
-                        src_product_pitch *= src_pitch[i];
+        } else {
+                size_t * cumul_dst_pitch = (size_t *)alloca((d-1)*sizeof(size_t));
+                size_t * cumul_src_pitch = (size_t *)alloca((d-1)*sizeof(size_t));
+                assert(dst_pitch[0] >= elem_number[0]);
+                assert(src_pitch[0] >= elem_number[0]);
+                cumul_dst_pitch[0] = elem_size * dst_pitch[0];
+                cumul_src_pitch[0] = elem_size * src_pitch[0];
+                for(int i = 1; i < d - 1; i++) {
+                        assert(dst_pitch[i] >= elem_number[i]);
+                        assert(src_pitch[i] >= elem_number[i]);
+                        cumul_dst_pitch[i] = dst_pitch[i] * cumul_dst_pitch[i-1];
+                        cumul_src_pitch[i] = src_pitch[i] * cumul_src_pitch[i-1];
                 }
-                for(int i = 0; i < elem_number[d-1]; i++) {
-                        aml_copy_nd(d-1, dst, dst_pitch, src, src_pitch, elem_number, elem_size);
-                        dst += dst_product_pitch;
-                        src += src_product_pitch;
-                }
+                aml_copy_nd_helper(d, dst, cumul_dst_pitch, src, cumul_src_pitch, elem_number, elem_size);
         }
         return 0;
 }
+
 
 static inline int aml_copy_t2d(void *dst, const size_t *dst_pitch, const void *src, const size_t *src_pitch, const size_t *elem_number, const size_t elem_size)
 {
