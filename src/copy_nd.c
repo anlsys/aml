db639f51925982ed9641343e2254c54480fd0a04
Brice Videau <brice.videau@imag.fr>
2018-10-01 14:12:22 -0500

    Simplified aml_copy_rtnd.

--- src/copy_nd.c
+++ src/copy_nd.c
@@ -2,17 +2,16 @@
 #include <assert.h>
 #include <stdlib.h>
 
-static inline int aml_copy_2d(void *dst, const size_t *cumul_dst_pitch, const void *src, const size_t *cumul_src_pitch, const size_t *elem_number, size_t elem_size)
+static inline void aml_copy_2d(void *dst, const size_t *cumul_dst_pitch, const void *src, const size_t *cumul_src_pitch, const size_t *elem_number, size_t elem_size)
 {
         for(int i = 0; i < elem_number[1]; i++) {
                 memcpy(dst, src, elem_number[0]*elem_size);
                 dst += cumul_dst_pitch[0];
                 src += cumul_src_pitch[0];
         }
-        return 0;
 }
 
-static inline int aml_copy_nd_helper(size_t d, void *dst, const size_t *cumul_dst_pitch, const void *src, const size_t *cumul_src_pitch, const size_t *elem_number, const size_t elem_size) {
+static inline void aml_copy_nd_helper(size_t d, void *dst, const size_t *cumul_dst_pitch, const void *src, const size_t *cumul_src_pitch, const size_t *elem_number, const size_t elem_size) {
         if(d == 2)
                 aml_copy_2d(dst, cumul_dst_pitch, src, cumul_src_pitch, elem_number, elem_size);
         else {
@@ -22,7 +21,6 @@ static inline int aml_copy_nd_helper(size_t d, void *dst, const size_t *cumul_ds
                         src += cumul_src_pitch[d-2];
                 }
         }
-        return 0;
 }
 
 int aml_copy_nd(size_t d, void *dst, const size_t *dst_pitch, const void *src, const size_t *src_pitch, const size_t *elem_number, const size_t elem_size)
@@ -50,14 +48,13 @@ int aml_copy_nd(size_t d, void *dst, const size_t *dst_pitch, const void *src, c
 }
 
 
-static inline int aml_copy_t2d(void *dst, const size_t *cumul_dst_pitch, const void *src, const size_t *cumul_src_pitch, const size_t *elem_number, const size_t elem_size)
+static inline void aml_copy_t2d(void *dst, const size_t *cumul_dst_pitch, const void *src, const size_t *cumul_src_pitch, const size_t *elem_number, const size_t elem_size)
 {
         for(int j = 0; j < elem_number[1]; j++)
                 for(int i = 0; i < elem_number[0]; i++)
                         memcpy(dst + j * elem_size + i * cumul_dst_pitch[0],
                                src + i * elem_size + j * cumul_src_pitch[0],
                                elem_size);
-        return 0;
 }
 
 // d[j + i * dp[0]]
@@ -66,20 +63,22 @@ static inline int aml_copy_t2d(void *dst, const size_t *cumul_dst_pitch, const v
 // s[i + j * sp[0]]
 // s[i + j * sp[0] + k * sp[0] * sp[1]]
 // s[i + j * sp[0] + k * sp[0] * sp[1] + l * sp[0] * sp[1] * sp[2]]
-int aml_copy_tnd_helper(size_t d, void *dst, size_t *cumul_dst_pitch, const void *src, const size_t *cumul_src_pitch, const size_t *elem_number, const size_t elem_size)
+static inline void aml_copy_tnd_helper(size_t d, void *dst, size_t *cumul_dst_pitch, const void *src, const size_t *cumul_src_pitch, const size_t *elem_number, const size_t elem_size)
 {
         if(d == 2)
                 aml_copy_t2d(dst, cumul_dst_pitch, src, cumul_src_pitch, elem_number, elem_size);
         else {
-                size_t dst_pitch = cumul_dst_pitch[d-3];
-                cumul_dst_pitch[d-3] = cumul_dst_pitch[d-2];
+                size_t * new_cumul_dst_pitch = (size_t *)alloca((d-1)*sizeof(size_t));
+                //process dimension d-1
+                for(int i = 0; i < d-3; i++)
+                        new_cumul_dst_pitch[i] = cumul_dst_pitch[i];
+                new_cumul_dst_pitch[d-3] = cumul_dst_pitch[d-2];
                 for(int i = 0; i < elem_number[d-1]; i++) {
-                        aml_copy_tnd_helper(d-1, dst, cumul_dst_pitch, src, cumul_src_pitch, elem_number, elem_size);
-                        dst += dst_pitch;
+                        aml_copy_tnd_helper(d-1, dst, new_cumul_dst_pitch, src, cumul_src_pitch, elem_number, elem_size);
+                        dst += cumul_dst_pitch[d-3];
                         src += cumul_src_pitch[d-2];
                 }
         }
-        return 0;
 }
 
 int aml_copy_tnd(size_t d, void *dst, const size_t *dst_pitch, const void *src, const size_t *src_pitch, const size_t *elem_number, const size_t elem_size)
@@ -111,36 +110,55 @@ int aml_copy_tnd(size_t d, void *dst, const size_t *dst_pitch, const void *src, 
 // s[i + l * sp[0]]
 // s[i + k * sp[0] + l * sp[0] * sp[1]]
 // s[i + j * sp[0] + k * sp[0] * sp[1] + l * sp[0] * sp[1] * sp[2]]
+static inline void aml_copy_rtnd_helper(size_t d, void *dst, size_t *cumul_dst_pitch, const void *src, size_t *cumul_src_pitch, size_t *elem_number, const size_t elem_size)
+{
+        if(d == 2)
+                aml_copy_t2d(dst, cumul_dst_pitch, src, cumul_src_pitch, elem_number, elem_size);
+        else {
+                size_t * new_cumul_dst_pitch = (size_t *)alloca((d-2)*sizeof(size_t));
+                size_t * new_cumul_src_pitch = (size_t *)alloca((d-2)*sizeof(size_t));
+                size_t * new_elem_number = (size_t *)alloca((d-1)*sizeof(size_t));
+                // process dimension 1
+                new_cumul_dst_pitch[0] = cumul_dst_pitch[0];
+                for(int i = 1; i < d-2; i++)
+                        new_cumul_dst_pitch[i] = cumul_dst_pitch[i+1];
+                for(int i = 0; i < d-2; i++)
+                        new_cumul_src_pitch[i] = cumul_src_pitch[i+1];
+                new_elem_number[0] = elem_number[0];
+                for(int i = 1; i < d-1; i++)
+                        new_elem_number[i] = elem_number[i+1];
+                for(int i = 0; i < elem_number[1]; i++) {
+                        aml_copy_rtnd_helper(d-1, dst, new_cumul_dst_pitch, src, new_cumul_src_pitch, new_elem_number, elem_size);
+                        dst += cumul_dst_pitch[1];
+                        src += cumul_src_pitch[0];
+                }
+        }
+}
+
 int aml_copy_rtnd(size_t d, void *dst, const size_t *dst_pitch, const void *src, const size_t *src_pitch, const size_t *elem_number, const size_t elem_size)
 {
         assert(d > 0);
         if(d == 1)
+        {
                 memcpy(dst, src, elem_number[0]*elem_size);
-        else if(d == 2)
-                aml_copy_t2d(dst, dst_pitch, src, src_pitch, elem_number, elem_size);
-        else {
-                assert( src_pitch[1] >= elem_number[1] );
-
-                size_t * new_dst_pitch = alloca((d-2)*sizeof(size_t));
-                size_t * new_src_pitch = alloca((d-2)*sizeof(size_t));
-                size_t * new_elem_number = alloca((d-1)*sizeof(size_t));
-                new_dst_pitch[0] = dst_pitch[0];
-                for(int i = 1; i < d-2; i++)
-                        new_dst_pitch[i] = dst_pitch[i+1];
-                new_dst_pitch[1] *= dst_pitch[1];
-                for(int i = 0; i < d-2; i++)
-                        new_src_pitch[i] = src_pitch[i+1];
-                new_src_pitch[0] *= src_pitch[0];
-                new_elem_number[0] = elem_number[0];
-                for(int i = 1; i < d-1; i++)
-                        new_elem_number[i] = elem_number[i+1];
-                size_t dst_product_pitch = elem_size*dst_pitch[0]*dst_pitch[1];
-                size_t src_product_pitch = elem_size*src_pitch[0];
-                for(int i = 0; i < elem_number[1]; i++) {
-                        aml_copy_rtnd(d-1, dst, new_dst_pitch, src, new_src_pitch, new_elem_number, elem_size);
-                        dst += dst_product_pitch;
-                        src += src_product_pitch;
+        } else {
+                size_t * cumul_dst_pitch = (size_t *)alloca((d-1)*sizeof(size_t));
+                size_t * cumul_src_pitch = (size_t *)alloca((d-1)*sizeof(size_t));
+                size_t * new_elem_number = (size_t *)alloca(d*sizeof(size_t));
+                assert(dst_pitch[0] >= elem_number[d-1]);
+                assert(src_pitch[0] >= elem_number[0]);
+                cumul_dst_pitch[0] = elem_size * dst_pitch[0];
+                cumul_src_pitch[0] = elem_size * src_pitch[0];
+                for(int i = 1; i < d - 1; i++) {
+                        assert(dst_pitch[i] >= elem_number[i-1]);
+                        assert(src_pitch[i] >= elem_number[i]);
+                        cumul_dst_pitch[i] = dst_pitch[i] * cumul_dst_pitch[i-1];
+                        cumul_src_pitch[i] = src_pitch[i] * cumul_src_pitch[i-1];
                 }
+                for(int i = 0; i < d; i++) {
+                        new_elem_number[i] = elem_number[i];
+                }
+                aml_copy_rtnd_helper(d, dst, cumul_dst_pitch, src, cumul_src_pitch, new_elem_number, elem_size);
         }
         return 0;
 }
