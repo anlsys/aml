e1fb7ccfc0735e9e3c701dec9e0607fd6118426d
Nicolas Denoyelle <ndenoyelle@anl.gov>
2019-03-15 10:47:15 -0500

    make test compilable. Do not pass yet

--- tests/dma/test_dma_linux_par.c
+++ tests/dma/test_dma_linux_par.c
@@ -16,39 +16,26 @@
 
 int main(int argc, char *argv[])
 {
-	AML_BINDING_SINGLE_DECL(binding);
 	AML_TILING_1D_DECL(tiling);
-	AML_ARENA_JEMALLOC_DECL(arena);
-	AML_AREA_LINUX_DECL(area);
 	AML_DMA_LINUX_PAR_DECL(dma);
-        aml_bitmap_decl(nodemask);
 	void *dst, *src;
 
 	/* library initialization */
 	aml_init(&argc, &argv);
 
 	/* initialize all the supporting struct */
-	assert(!aml_binding_init(&binding, AML_BINDING_TYPE_SINGLE, 0));
 	assert(!aml_tiling_init(&tiling, AML_TILING_TYPE_1D, TILESIZE*PAGE_SIZE,
 				TILESIZE*PAGE_SIZE*NBTILES));
-	aml_bitmap_zero(nodemask);
-	aml_bitmap_set(nodemask, 0);
-	assert(!aml_arena_jemalloc_init(&arena, AML_ARENA_JEMALLOC_TYPE_REGULAR));
 
-	assert(!aml_area_linux_init(&area,
-				    AML_AREA_LINUX_MANAGER_TYPE_SINGLE,
-				    AML_AREA_LINUX_MBIND_TYPE_REGULAR,
-				    AML_AREA_LINUX_MMAP_TYPE_ANONYMOUS,
-				    &arena, MPOL_BIND, nodemask));
 
 	size_t maxrequests = NBTILES;
 	size_t maxthreads = 4;
 	assert(!aml_dma_linux_par_init(&dma, maxrequests, maxthreads));
 
 	/* allocate some memory */
-	src = aml_area_malloc(&area, TILESIZE*PAGE_SIZE*NBTILES);
+	assert(aml_area_malloc(aml_area_host_private, &src, TILESIZE*PAGE_SIZE*NBTILES, 0) == AML_AREA_SUCCESS);
 	assert(src != NULL);
-	dst = aml_area_malloc(&area, TILESIZE*PAGE_SIZE*NBTILES);
+	assert(aml_area_malloc(aml_area_host_private, &dst, TILESIZE*PAGE_SIZE*NBTILES, 0) == AML_AREA_SUCCESS);
 	assert(dst != NULL);
 
 	memset(src, 42, TILESIZE*PAGE_SIZE*NBTILES);
@@ -60,17 +47,11 @@ int main(int argc, char *argv[])
 
 	assert(!memcmp(src, dst, TILESIZE*PAGE_SIZE*NBTILES));
 
-	/* now move it by pages */
-	for(int i = 0; i < NBTILES; i++)
-		aml_dma_move(&dma, &area, &tiling, src, i);
-
 	/* delete everything */
 	aml_dma_linux_par_destroy(&dma);
-	aml_area_free(&area, dst);
-	aml_area_free(&area, src);
-	aml_area_linux_destroy(&area);
+	aml_area_free(aml_area_host_private, dst);
+	aml_area_free(aml_area_host_private, src);
 	aml_tiling_destroy(&tiling, AML_TILING_TYPE_1D);
-	aml_binding_destroy(&binding, AML_BINDING_TYPE_SINGLE);
 
 	aml_finalize();
 	return 0;
