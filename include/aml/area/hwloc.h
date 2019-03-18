f537960a3ad963b0599275128f759d8154c3f451
Nicolas Denoyelle <ndenoyelle@anl.gov>
2019-03-18 17:11:05 -0500

    remove useless include

--- include/aml/area/hwloc.h
+++ include/aml/area/hwloc.h
@@ -1,9 +1,6 @@
 #ifndef AML_AREA_HWLOC_H
 #define AML_AREA_HWLOC_H
 
-/** struct aml_bitmap can be translated to hwloc_nodeset_t and hwloc_bitmap_t **/
-#include <aml/utils/hwloc.h>
-
 /** 
  * Use linux areas allocation with hwloc binding capabilities for process wide
  * memory operations.
