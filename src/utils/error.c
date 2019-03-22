880284f3a44db559054e76bdf4d998602216de73
Swann Perarnau <swann@anl.gov>
2019-03-22 15:25:12 -0500

    [style] fix repoquality checks after reorg
    The source repo has been reorganized, but the repoquality checks were
    not updated. Also make sure that style checks happen early, to fail the
    CI quickly.

--- src/utils/error.c
+++ src/utils/error.c
@@ -1,5 +1,15 @@
+/*******************************************************************************
+ * Copyright 2019 UChicago Argonne, LLC.
+ * (c.f. AUTHORS, LICENSE)
+ *
+ * This file is part of the AML project.
+ * For more info, see https://xgitlab.cels.anl.gov/argo/aml
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+*******************************************************************************/
+
+#include "aml.h"
 #include <stdio.h>
-#include "aml/utils/error.h"
 
 static const char* aml_error_strings[-AML_ERROR_MAX] = {
 	[AML_SUCCESS] =
