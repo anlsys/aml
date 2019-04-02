e243f7eb239ff8799365db21633f92534c4cfb0e
Swann Perarnau <swann@anl.gov>
2019-04-02 16:20:28 -0500

    [style/refactor] posixify error handling
    In POSIX land, error codes are positive integers, returned by functions
    as a negative value, but assigned to errno as a positive value.
    
    Use that chance to checkpatch the files.

--- include/aml/utils/error.h
+++ include/aml/utils/error.h
@@ -12,47 +12,36 @@
 #define AML_ERROR_H
 
 /**
- * Variable set by aml function calls. aml_errno should be checked after an aml function call
- * returning an error prior to any other aml call that may overwrite it.
+ * Variable set by aml function calls. aml_errno should be checked after an aml
+ * function call returning an error and prior to any other aml call that may
+ * overwrite it.
  **/
 extern int aml_errno;
 
 /**
  * Get a string description of an aml error.
- * "errno": the aml error number.
- * Returns a on stack string with error description.
+ * @param errno: the aml error number.
+ * Returns a static string describing the error.
  **/
-const char* aml_strerror(const int errno);
+const char *aml_strerror(const int errno);
 
 /**
  * Print error on standard error output.
  * "msg": A message to prepend to error message.
  **/
-void aml_perror(const char * msg);
+void aml_perror(const char *msg);
 
-/* Generic value for success */
-#define AML_SUCCESS       0
-/* Generic value for failure */
-#define AML_FAILURE      -1
-
-/************************************ 
- * Area error codes  -2 .. -32 
- ************************************/
-
-/* Invalid argument provided */
-#define AML_AREA_EINVAL       -2
-/* Function not implemented for this type of area */
-#define AML_AREA_ENOTSUP      -3
-/* Allocation failed */
-#define AML_AREA_ENOMEM       -4 
-/* One arguent is out of allowed bounds */
-#define AML_AREA_EDOM         -5
-
-/************************************ 
- * error bound
- ************************************/
-
-/* Last error */
-#define AML_ERROR_MAX         -7
+/**
+ * Error codes.
+ * As is quite common in C code, error code values are defined in positive,
+ * but are returned in negative.
+ */
+#define AML_SUCCESS	0	/* Generic value for success */
+#define AML_FAILURE	1	/* Generic value for failure */
+#define AML_ENOMEM	2	/* No enough memory available. */
+#define AML_EINVAL	3	/* Invalid argument provided */
+#define AML_EDOM	4	/* value out of bound. */
+#define AML_ENOTSUP	5	/* Operation not supported */
+#define AML_ERROR_MAX	6	/* Max allowed value for errors. */
 
 #endif
