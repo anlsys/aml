ed30f0ab875fd53e547ac7198ebc557d7aac092d
Swann Perarnau <swann@anl.gov>
2019-09-04 16:56:12 -0500

    [feature/refactor] second version of tilings
    Refactor the tilings to become generic to N dimensions, and interfacing
    with the newly added layouts.
    
    The main idea for this version of tilings is to provide an index into a
    partitioning of a source layout into sub-layouts of smaller sizes.

---
+++ include/aml/tiling/native.h
@@ -0,0 +1,30 @@
+/*************************************************************************
+ * Copyright 2019 UChicago Argonne, LLC.
+ * (c.f. AUTHORS, LICENSE)
+ *
+ * This file is part of the AML project.
+ * For more info, see https://xgitlab.cels.anl.gov/argo/aml
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+**************************************************************************/
+
+#ifndef AML_TILING_NATIVE_H
+#define AML_TILING_NATIVE_H
+
+/**
+ * @defgroup aml_tiling_native "AML Tiling Internal API"
+ * @brief API for internal management of tilings.
+ *
+ * #include <aml/tiling/native.h>
+ * @{
+ **/
+
+struct aml_layout *aml_tiling_index_native(const struct aml_tiling *tiling,
+					   const size_t *coords);
+
+int aml_tiling_dims_native(const struct aml_tiling *tiling, size_t *dims);
+/**
+ * @}
+ **/
+
+#endif // AML_TILING_NATIVE_H
