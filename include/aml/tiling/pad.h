dfac08009fa5b855e63381e4bf3c93eddc643580
Swann Perarnau <swann@anl.gov>
2019-10-01 16:14:47 -0500

    [feature] add padded tilings
    Continue the work of merging the new tilings API, now with padded
    tilings.

---
+++ include/aml/tiling/pad.h
@@ -0,0 +1,50 @@
+/*******************************************************************************
+ * Copyright 2019 UChicago Argonne, LLC.
+ * (c.f. AUTHORS, LICENSE)
+ *
+ * This file is part of the AML project.
+ * For more info, see https://xgitlab.cels.anl.gov/argo/aml
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+*******************************************************************************/
+
+#ifndef AML_TILING_PAD_H
+#define AML_TILING_PAD_H 1
+
+/**
+ * @defgroup aml_tiling_pad "AML Padded Tiling"
+ * @brief tiling with padding at the border
+ *
+ * Implementation of a tiling for which the border tiles are padded up to the
+ * requested size.
+ * @{
+ **/
+
+/** Initialized structure containing operations for a tiling in column order.**/
+extern struct aml_tiling_ops aml_tiling_pad_column_ops;
+
+/** Initialized structure containing operations for a tiling in row order. **/
+extern struct aml_tiling_ops aml_tiling_pad_row_ops;
+
+struct aml_tiling_pad {
+	int tags;
+	const struct aml_layout *layout;
+	size_t ndims;
+	size_t *tile_dims;
+	size_t *dims;
+	size_t *border_tile_dims;
+	size_t *pad;
+	void *neutral;
+};
+
+int aml_tiling_pad_create(struct aml_tiling **t, int tags,
+			  const struct aml_layout *l, size_t ndims,
+			  const size_t *tile_dims, void *neutral);
+
+void aml_tiling_pad_destroy(struct aml_tiling **t);
+
+/**
+ * @}
+ **/
+
+#endif /* AML_TILING_PAD_H */
