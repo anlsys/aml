773f131fbbf763f28f744ac82c22f4942c845340
Swann Perarnau <swann@anl.gov>
2020-10-05 11:12:25 -0500

    [fix] move common replicaset test in test lib
    Move common code in a different file, add a header just for the tests.
    Also cleanup a little bit the test Makefile.am.

--- tests/replicaset/test_hwloc.c
+++ tests/replicaset/test_hwloc.c
@@ -8,37 +8,11 @@
  * SPDX-License-Identifier: BSD-3-Clause
  ******************************************************************************/
 
+#include "test_replicaset.h"
+
 #include <stdlib.h>
 
-#include "aml.h"
-
-#include "aml/higher/replicaset.h"
 #include "aml/higher/replicaset/hwloc.h"
-
-void test_replicaset(struct aml_replicaset *replicaset,
-                     const void *src,
-                     int (*comp)(const void *, const void *, size_t))
-{
-	// Basic tests
-	assert(replicaset != NULL);
-	assert(replicaset->ops != NULL);
-	assert(replicaset->ops->init != NULL);
-	assert(replicaset->ops->sync != NULL);
-	assert(replicaset->n > 0);
-	for (unsigned i = 0; i < replicaset->n; i++)
-		assert(replicaset->replica[i] != NULL);
-
-	// Check sync
-	assert(aml_replicaset_sync(replicaset, 0) == AML_SUCCESS);
-	for (unsigned i = 1; i < replicaset->n; i++)
-		assert(!comp(replicaset->replica[i], replicaset->replica[0],
-		             replicaset->size));
-
-	// Check init
-	assert(aml_replicaset_init(replicaset, src) == AML_SUCCESS);
-	for (unsigned i = 0; i < replicaset->n; i++)
-		assert(!comp(replicaset->replica[i], src, replicaset->size));
-}
 
 const size_t size = 1 << 20; // 1 Mo
 void *src;
---
+++ tests/replicaset/test_replicaset.h
@@ -0,0 +1,22 @@
+/*******************************************************************************
+ * Copyright 2019 UChicago Argonne, LLC.
+ * (c.f. AUTHORS, LICENSE)
+ *
+ * This file is part of the AML project.
+ * For more info, see https://xgitlab.cels.anl.gov/argo/aml
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ ******************************************************************************/
+
+#include "aml.h"
+
+#include "aml/higher/replicaset.h"
+
+#ifndef TEST_REPLICASET_H
+#define TEST_REPLICASET_H 1
+
+void test_replicaset(struct aml_replicaset *replicaset,
+                     const void *src,
+                     int (*comp)(const void *, const void *, size_t));
+
+#endif
