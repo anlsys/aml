12984331fb49db628e012124a2a46184f03ad95a
Nicolas Denoyelle <ndenoyelle@anl.gov>
2021-07-21 12:30:44 -0500

    [cleanup] tests factorization

--- tests/dma/test_cuda.c
+++ tests/dma/test_cuda.c
@@ -8,104 +8,37 @@
  * SPDX-License-Identifier: BSD-3-Clause
  ******************************************************************************/
 
-#include <cuda.h>
-#include <cuda_runtime.h>
+#include "test_dma.h"
 
 #include "aml.h"
 
+#include "aml/area/cuda.h"
 #include "aml/dma/cuda.h"
-#include "aml/layout/dense.h"
 
-// Data
-#define size (1 << 24) // 16 MB
-void *host_data;
-void *device_data;
-
-// Layout
-const size_t ndims = 1;
-const size_t dims = size;
-const size_t element_size = 1;
-const size_t stride = 0;
-const size_t pitch = 1;
-struct aml_layout *host_layout;
-struct aml_layout *device_layout;
-
-// Dma
-struct aml_dma *host_device;
-struct aml_dma *device_host;
-
-void setup()
+int aml_memcpy_cuda(struct aml_layout *dst,
+                    const struct aml_layout *src,
+                    void *arg)
 {
-	// Data
-	host_data = malloc(size);
-	assert(host_data != NULL);
-	assert(cudaMalloc(&device_data, size) == cudaSuccess);
+	struct aml_dma_cuda_op_arg *op_arg = (struct aml_dma_cuda_op_arg *)arg;
+	size_t size = (size_t)op_arg->op_arg;
 
-	// Layout
-	assert(aml_layout_dense_create(&host_layout, host_data,
-	                               AML_LAYOUT_ORDER_COLUMN_MAJOR,
-	                               element_size, ndims, &dims, &stride,
-	                               &pitch) == AML_SUCCESS);
-	assert(aml_layout_dense_create(&device_layout, device_data,
-	                               AML_LAYOUT_ORDER_COLUMN_MAJOR,
-	                               element_size, ndims, &dims, &stride,
-	                               &pitch) == AML_SUCCESS);
-
-	// Dma
-	assert(aml_dma_cuda_create(&host_device, cudaMemcpyHostToDevice) ==
-	       AML_SUCCESS);
-	assert(aml_dma_cuda_create(&device_host, cudaMemcpyDeviceToHost) ==
-	       AML_SUCCESS);
-}
-
-void teardown()
-{
-	// Data
-	free(host_data);
-	cudaFree(device_data);
-
-	// Layout
-	aml_layout_destroy(&host_layout);
-	aml_layout_destroy(&device_layout);
-
-	// Dma
-	aml_dma_cuda_destroy(&host_device);
-	aml_dma_cuda_destroy(&device_host);
-}
-
-// Fill host_data and device_data
-void initialize_buffers(int host_val, int device_val)
-{
-	memset(host_data, host_val, size);
-	assert(cudaMemset(device_data, device_val, size) == cudaSuccess);
+	if (op_arg->data->kind == cudaMemcpyDeviceToDevice)
+		return -AML_ENOTSUP;
+	else {
+		if (cudaMemcpyAsync(dst, src, size, op_arg->data->kind,
+		                    op_arg->data->stream) != cudaSuccess)
+			return -AML_FAILURE;
+	}
+	return AML_SUCCESS;
 }
 
 int main(int argc, char **argv)
 {
-	aml_init(&argc, &argv);
-
+	assert(aml_init(&argc, &argv) == AML_SUCCESS);
 	if (!aml_support_backends(AML_BACKEND_CUDA))
 		return 77;
-	setup();
 
-	void *check = malloc(size);
+	test_dma_memcpy(&aml_area_cuda, NULL, &aml_dma_cuda, aml_memcpy_cuda);
 
-	// Dma from device to host.
-	initialize_buffers(1, 0);
-	assert(aml_dma_copy_custom(device_host, host_layout, device_layout,
-	                           aml_dma_cuda_copy_1D, NULL) == AML_SUCCESS);
-	memset(check, 0, size);
-	assert(!memcmp(check, host_data, size));
-
-	// Dma from host to device.
-	initialize_buffers(1, 0);
-	assert(aml_dma_copy_custom(host_device, device_layout, host_layout,
-	                           aml_dma_cuda_copy_1D, NULL) == AML_SUCCESS);
-	assert(cudaMemcpy(check, device_data, size, cudaMemcpyDeviceToHost) ==
-	       cudaSuccess);
-	assert(!memcmp(check, host_data, size));
-
-	free(check);
-	teardown();
 	aml_finalize();
 }
---
+++ tests/dma/test_linux.c
@@ -0,0 +1,33 @@
+/*******************************************************************************
+ * Copyright 2019 UChicago Argonne, LLC.
+ * (c.f. AUTHORS, LICENSE)
+ *
+ * This file is part of the AML project.
+ * For more info, see https://github.com/anlsys/aml
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ ******************************************************************************/
+
+#include "test_dma.h"
+
+#include "aml.h"
+
+#include "aml/area/linux.h"
+#include "aml/dma/linux.h"
+
+int aml_memcpy_linux(struct aml_layout *dst,
+                     const struct aml_layout *src,
+                     void *arg)
+{
+	memcpy(dst, src, (size_t)arg);
+	return AML_SUCCESS;
+}
+
+int main(int argc, char **argv)
+{
+	assert(aml_init(&argc, &argv) == AML_SUCCESS);
+
+	test_dma_memcpy(&aml_area_linux, NULL, aml_dma_linux, aml_memcpy_linux);
+
+	aml_finalize();
+}
