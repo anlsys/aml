46d63d29de7ee9fa4229ec9faf05704fcd84bc57
Swann Perarnau <swann@anl.gov>
2023-04-21 09:29:08 -0500

    [fix] ci updates, helgrind (#227)
    Fix broken CI because of base image updates:
     - hwloc update deprecated some functions
    Valgrind tests don't work anymore, will be fixed later.

--- default.nix
+++ default.nix
@@ -1,23 +1,6 @@
-{ nixpkgs ?
-  builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/20.09.tar.gz"
+{ pkgs ? import (builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/22.05.tar.gz") {}
 }:
-let
-  pkgs = import nixpkgs {
-    overlays = [
-      (_: pkgs: {
-
-        aml = let
-          f = { stdenv, src, autoreconfHook, git, pkgconf, numactl, hwloc }:
-            stdenv.mkDerivation {
-              src = ./.;
-              name = "aml";
-              nativeBuildInputs = [ autoreconfHook pkgconf git ];
-              buildInputs = [ hwloc numactl ];
-            };
-        in pkgs.lib.callPackageWith pkgs f { };
-
-      })
-    ];
-  };
-
-in pkgs
+pkgs // rec {
+  stdenv = pkgs.gcc12Stdenv;
+  aml = pkgs.callPackage ./nix/aml.nix { inherit stdenv; };
+}
