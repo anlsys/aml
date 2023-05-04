04fd263277d3bdfa7cd60613d179acc556cbab6e
Kamil Iskra <iskra@mcs.anl.gov>
2023-05-04 09:50:03 -0500

    Fix compilation errors
    bw function was off by 3 orders of magnitude.
    
    Bail out early if ZE backend not supported.
    
    Reduce the number of iterations to keep the execution time reasonable.

--- tests/dma/test-aml-dma-ze.c
+++ tests/dma/test-aml-dma-ze.c
@@ -13,32 +13,38 @@
 
 #include <aml.h>
 
+#include <aml/area/ze.h>
 #include <aml/dma/ze.h>
+
+#define min(a,b) \
+   ({ __typeof__ (a) _a = (a); \
+       __typeof__ (b) _b = (b); \
+     _a < _b ? _a : _b; })
 
 static int64_t timediff(struct timespec end, struct timespec start)
 {
 	int64_t e = end.tv_sec * 1e9 + end.tv_nsec;
-	int64_t s = s.tv_sec * 1e9 + s.tv_nsec;
+	int64_t s = start.tv_sec * 1e9 + start.tv_nsec;
 	return e - s;
 }
 
 static double bw(size_t size, int64_t time)
 {
-	return (1e-3 * size) / time;
+	return size / (double)time;
 }
 
-int do_copy_1wait(
+int do_copy_1way(
         struct aml_dma *dma, void *dest, void *src, size_t size, int repeat)
 {
 	int64_t mintime = INT64_MAX;
 	struct timespec start, end;
 	for (int i = 0; i < repeat; i++) {
 		clock_gettime(CLOCK_MONOTONIC, &start);
-		aml_dma_copy_custom(dma, dest, src, aml_dma_ze_memcpy_op, size);
+		aml_dma_copy_custom(dma, dest, src, aml_dma_ze_memcpy_op, (void*)size);
 		clock_gettime(CLOCK_MONOTONIC, &end);
 		mintime = min(mintime, timediff(end, start));
 	}
-	fprintf(stdout, "BW: %gGBytes/s\n", bw(size, mintime));
+	printf("BW: %gGBytes/s\n", bw(size, mintime));
 	return 0;
 }
 
@@ -57,13 +63,13 @@ int do_copy_chunks(struct aml_dma *dma,
 		for (int j = 0; j < chunks; j++) {
 			aml_dma_async_copy_custom(dma, NULL, dest + (j * cksz),
 			                          src + (j * cksz),
-			                          aml_dma_ze_memcpy_op, cksz);
+			                          aml_dma_ze_memcpy_op, (void*)cksz);
 		}
 		aml_dma_barrier(dma);
 		clock_gettime(CLOCK_MONOTONIC, &end);
 		mintime = min(mintime, timediff(end, start));
 	}
-	fprintf(stdout, "BW: %gGBytes/s\n", bw(size, mintime));
+	printf("BW: %gGBytes/s\n", bw(size, mintime));
 	return 0;
 }
 
@@ -80,14 +86,14 @@ int do_copy_2barrier(struct aml_dma *dma,
 	for (int i = 0; i < repeat; i++) {
 		clock_gettime(CLOCK_MONOTONIC, &start);
 		aml_dma_async_copy_custom(dma, NULL, dest1, src1,
-		                          aml_dma_ze_memcpy_op, size);
+		                          aml_dma_ze_memcpy_op, (void*)size);
 		aml_dma_async_copy_custom(dma, NULL, dest2, src2,
-		                          aml_dma_ze_memcpy_op, size);
+		                          aml_dma_ze_memcpy_op, (void*)size);
 		aml_dma_barrier(dma);
 		clock_gettime(CLOCK_MONOTONIC, &end);
 		mintime = min(mintime, timediff(end, start));
 	}
-	fprintf(stdout, "BW: %gGBytes/s\n", bw(size, mintime));
+	printf("BW: %gGBytes/s\n", bw(size, mintime));
 	return 0;
 }
 
@@ -108,16 +114,16 @@ int do_copy_2chunks(struct aml_dma *dma,
 		for (int j = 0; j < chunks; j++) {
 			aml_dma_async_copy_custom(dma, NULL, dest1 + (j * cksz),
 			                          src1 + (j * cksz),
-			                          aml_dma_ze_memcpy_op, cksz);
+			                          aml_dma_ze_memcpy_op, (void*)cksz);
 			aml_dma_async_copy_custom(dma, NULL, dest2 + (j * cksz),
 			                          src2 + (j * cksz),
-			                          aml_dma_ze_memcpy_op, cksz);
+			                          aml_dma_ze_memcpy_op, (void*)cksz);
 		}
 		aml_dma_barrier(dma);
 		clock_gettime(CLOCK_MONOTONIC, &end);
 		mintime = min(mintime, timediff(end, start));
 	}
-	fprintf(stdout, "BW: %gGBytes/s\n", bw(size, mintime));
+	printf("BW: %gGBytes/s\n", bw(size, mintime));
 	return 0;
 }
 
@@ -127,31 +133,34 @@ int main(int argc, char *argv[])
 	const size_t N = bytes / sizeof(double);
 	const int repeats = 100;
 
-	aml_init(&argc, &argv);
+	assert(aml_init(&argc, &argv) == AML_SUCCESS);
+	if (!aml_support_backends(AML_BACKEND_ZE))
+		return 77;
+
 
 	double *host =
 	        aml_area_mmap(aml_area_ze_host, N * sizeof(double), NULL);
 	double *device =
 	        aml_area_mmap(aml_area_ze_device, N * sizeof(double), NULL);
 
-	fprintf("Default ZE DMA\n");
-	fprintf("H2D: 1C-wait\n");
+	printf("Default ZE DMA\n");
+	printf("H2D: 1C-wait\n");
 	do_copy_1way(aml_dma_ze_default, device, host, bytes, repeats);
 
-	for (size_t i = 1; i < 20; i++) {
-		fprintf("Default ZE DMA\n");
-		fprintf("H2D: Chunks: %zu\n", i);
+	for (size_t i = 1; i < 5; i++) {
+		printf("Default ZE DMA\n");
+		printf("H2D: Chunks: %zu\n", i);
 		do_copy_chunks(aml_dma_ze_default, device, host, bytes, i,
 		               repeats);
 	}
 
-	fprintf("Default ZE DMA\n");
-	fprintf("D2H: 1C-wait\n");
+	printf("Default ZE DMA\n");
+	printf("D2H: 1C-wait\n");
 	do_copy_1way(aml_dma_ze_default, host, device, bytes, repeats);
 
-	for (size_t i = 1; i < 100; i++) {
-		fprintf("Default ZE DMA\n");
-		fprintf("D2H: Chunks: %zu\n", i);
+	for (size_t i = 1; i < 5; i++) {
+		printf("Default ZE DMA\n");
+		printf("D2H: Chunks: %zu\n", i);
 		do_copy_chunks(aml_dma_ze_default, host, device, bytes, i,
 		               repeats);
 	}
@@ -161,14 +170,14 @@ int main(int argc, char *argv[])
 	double *device2 =
 	        aml_area_mmap(aml_area_ze_device, N * sizeof(double), NULL);
 
-	fprintf("Default ZE DMA\n");
-	fprintf("2Way: 1C-barrier\n");
+	printf("Default ZE DMA\n");
+	printf("2Way: 1C-barrier\n");
 	do_copy_2barrier(aml_dma_ze_default, host, device, device2, host2,
 	                 bytes, repeats);
 
-	for (size_t i = 1; i < 100; i++) {
-		fprintf("Default ZE DMA\n");
-		fprintf("2Way: Chunks: %zu\n", i);
+	for (size_t i = 1; i < 5; i++) {
+		printf("Default ZE DMA\n");
+		printf("2Way: Chunks: %zu\n", i);
 		do_copy_2chunks(aml_dma_ze_default, host, device, device2,
 		                host2, bytes, i, repeats);
 	}
